2a20c14adf8e149a694df2ff741d769b282c248a
Natty <danilodevs@gmail.com>
2016-05-30 14:35:10 -0400

    Made gulpfile "Beautiful"

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,25 @@
-var gulp = require( "gulp" );
-var ts = require( "gulp-typescript" );
-var uglify = require( "gulp-uglify" );
+var gulp = require("gulp");
+var ts = require("gulp-typescript");
+var uglify = require("gulp-uglify");
 
-var paths = 
-{
-    node : [
+var paths = {
+    node: [
         "./src/**/*.ts",
         "./typings/index.d.ts"
     ]
 };
 
-var tsProject = ts.createProject( "tsconfig.json" );
+var tsProject = ts.createProject("tsconfig.json");
 
-gulp.task( "node", () =>
-{
-    return gulp.src( paths.node )
-        .pipe( ts( tsProject ) )
+gulp.task("node", () => {
+    return gulp.src(paths.node)
+        .pipe(ts(tsProject))
         // .pipe( uglify() )
-        .pipe( gulp.dest( "./build/" ) );
+        .pipe(gulp.dest("./build/"));
 })
 
-gulp.task( "watch", () =>
-{
-    gulp.watch( paths.node, [ "node" ] );
+gulp.task("watch", () => {
+    gulp.watch(paths.node, ["node"]);
 })
 
-gulp.task( "default", [ "watch", "node" ] );
+gulp.task("default", ["watch", "node"]);
