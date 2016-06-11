2e4354bb6d0af8b9935b7c837937701d5dc25ddc
dandevs <danilodevs@gmail.com>
2016-06-11 10:52:12 -0400

    Changed structure for web

--- gulpfile.js
+++ gulpfile.js
@@ -3,38 +3,38 @@ var ts = require("gulp-typescript");
 var sourcemaps = require("gulp-sourcemaps");
 
 var paths = {
-    node: [
-        "./src/**/*.ts",
-        "./typings/index.d.ts"
-    ],
-    package: "./src/package.json",
-    build: "./build",
-    sourcemaps : "sourcemaps"
+	node: [
+		"./src/ts/**/*.ts",
+		"./typings/tsd.d.ts"
+	],
+	package: "./src/package.json",
+	build: "./build",
+	sourcemaps : "sourcemaps"
 };
 
 var tsProject = ts.createProject("tsconfig.json");
 
 gulp.task("typescript", () => {
-    var tsResult = gulp.src(paths.node)
-        .pipe(sourcemaps.init())
-        .pipe(ts("./tsconfig.json"))
+	var tsResult = gulp.src(paths.node)
+		.pipe(sourcemaps.init())
+		.pipe(ts("./tsconfig.json"))
  
-        tsResult.dts
-            .pipe(gulp.dest(paths.build))
+		tsResult.dts
+			.pipe(gulp.dest(paths.build))
  
-        return tsResult.js
-        .pipe(sourcemaps.write(paths.sourcemaps))
-        .pipe(gulp.dest(paths.build));
+		return tsResult.js
+		.pipe(sourcemaps.write(paths.sourcemaps))
+		.pipe(gulp.dest(paths.build));
 });
 
 gulp.task("package.json", () => {
-    return gulp.src(paths.package)
-        .pipe(gulp.dest(paths.build));
+	return gulp.src(paths.package)
+		.pipe(gulp.dest(paths.build));
 });
 
 gulp.task("watch", () => {
-    gulp.watch(paths.node, ["typescript"]);
-    gulp.watch(paths.package, ["package.json"]);
+	gulp.watch(paths.node, ["typescript"]);
+	gulp.watch(paths.package, ["package.json"]);
 });
 
-gulp.task("default", ["watch", "typescript", "package.json"]);
+gulp.task("default", ["watch", "typescript"]);
