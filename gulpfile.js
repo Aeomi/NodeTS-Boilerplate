bfe6168e0663c4619c3b46ca233055886d956617
dandevs <danilodevs@gmail.com>
2016-06-12 15:09:47 -0400

    Minify and concat css

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require("gulp");
 var ts = require("gulp-typescript");
 var sourcemaps = require("gulp-sourcemaps");
 var uglifyJS = require("gulp-uglify");
+var uglifyCSS = require("gulp-uglifycss");
 var concat = require("gulp-concat");
 var es = require("event-stream");
 
@@ -15,6 +16,9 @@ var paths = {
 	venderInJS: "./src/www/js/**/*.js",
 	venderInTS: "./src/www/ts/**/*.ts",
 	vendorOut: "./dist/www/js",
+
+	cssIn : "./src/www/css/**/*.css",
+	cssOut : "./dist/www/css",
 	sourcemaps : "sourcemaps"
 };
 
@@ -38,16 +42,25 @@ gulp.task("vendor.js", () => {
 		gulp.src(paths.venderInJS),
 		
 		gulp.src(paths.venderInTS)
-		.pipe(ts({target:"ES5"}))
+			.pipe(ts({target:"ES5"}))
 	)
 	.pipe(concat("vendor.js"))
 	.pipe(uglifyJS())
 	.pipe(gulp.dest(paths.vendorOut));
+});
+
+gulp.task("style.css", () => {
+	return gulp.src(paths.cssIn)
+		.pipe(concat("style.css"))
+		.pipe(uglifyCSS())
+		.pipe(gulp.dest(paths.cssOut));
 })
 
 gulp.task("watch", () => {
 	gulp.watch(paths.node, ["typescript"]);
 	gulp.watch(paths.venderInJS, ["vendor.js"]);
+	gulp.watch(paths.venderInTS, ["vendor.js"]);
+	gulp.watch(paths.cssIn, ["style.css"]);
 });
 
-gulp.task("default", ["watch", "typescript", "vendor.js"]);
+gulp.task("default", ["watch", "typescript", "vendor.js", "style.css"]);
